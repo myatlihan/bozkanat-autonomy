cac3145d53c5232c4d097364f2817b9819b1e4af
Yasin Atlıhan <myatlihan@gmail.com>
2026-07-10 15:11:26 +0300

    Kullanılmayan değişkenlerden arındırıldı.

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
-"""
-Project Configuration
-"""
+'''
+Project configuration
+'''
 
-# ==========================
-# Camera
-# ==========================
-
+#Camera
 FRAME_WIDTH = 640
 FRAME_HEIGHT = 480
 
@@ -13,25 +10,18 @@ FRAME_CENTER_X = FRAME_WIDTH // 2
 FRAME_CENTER_Y = FRAME_HEIGHT // 2
 
 
-# ==========================
-# YOLO
-# ==========================
-
-MODEL_PATH = "models/best.pt"
-
+#YOLO
+MODEL_PATH = 'models/bets.pt'
 CONFIDENCE_THRESHOLD = 0.50
-
 IMAGE_SIZE = 640
 
-# ==========================
-# Target Classes
-# ==========================
 
-BLUE_SQUARE = "blue_square"
-RED_SQUARE = "red_square"
+#Target Classes
+BLUE_SQUARE = 'blue_square'
+RED_SQUARE = 'red_square'
 
-BLUE_HEXAGON = "blue_hexagon"
-RED_TRIANGLE = "red_triangle"
+BLUE_HEXAGON = 'blue_hexagon'
+RED_TRIANGLE = 'red_triangle'
 
 ALL_TARGETS = [
     BLUE_SQUARE,
@@ -45,31 +35,9 @@ MISSION_TARGETS = [
     RED_SQUARE
 ]
 
-# ==========================
-# Tracker
-# ==========================
 
-CENTER_THRESHOLD = 20          # px
+#Tracker
+CENTER_THRESHOLD = 20
 LOCK_FRAME_COUNT = 5
 LOST_FRAME_COUNT = 10
 
-# ==========================
-# Servo
-# ==========================
-
-SERVO_PIN = 18
-
-SERVO_RELEASE_ANGLE = 90
-SERVO_IDLE_ANGLE = 0
-
-# ==========================
-# Debug
-# ==========================
-
-SHOW_FPS = True
-
-SHOW_CENTER = True
-
-SHOW_BBOX = True
-
-PRINT_DETECTIONS = False
